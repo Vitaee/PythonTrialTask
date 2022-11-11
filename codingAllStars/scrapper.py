faf2b478335982aa2912bdac8df65a93207de7cc
Can İlgu <canilguu@gmail.com>
2022-11-11 18:26:05 +0300

    some bug fixes

--- codingAllStars/scrapper.py
+++ codingAllStars/scrapper.py
@@ -20,6 +20,8 @@ async def scrape_course_details(url):
 
             html = BeautifulSoup(body, 'html.parser')
 
+            print("\n\n", url, "\n\n")
+
             lecture_details_html = html.find('div','rc-CDPSchemaMarkup')
             if lecture_details_html is not None:
                 if lecture_details_html.find('script').text is not None:
@@ -29,59 +31,67 @@ async def scrape_course_details(url):
                 try:
                     lecture_details_html = html.find('div', class_='rc-S12nXDPSchemaMarkup').find('script').text
                 except AttributeError as e:
-                    lecture_details_html = html.find('script', attrs={"type":"application/ld+json"}).text
-
-            lecture_details_json = json.loads(lecture_details_html)
-
-            
-            try:
-                temp_json["Course Name"] = lecture_details_json["@graph"][1]["name"]
-            except KeyError as e:
-                temp_json["Course Name"] = ""
-
-            try:
-                temp_json["Course Provider"] = lecture_details_json["@graph"][1]["provider"]["name"]
-            except:
-                try:
-                    temp_json["Course Provider"] = lecture_details_json["@graph"][1]["provider"][0]["name"]
-                except KeyError as e:
-                    try: 
-                        temp_json["Course Provider"] = html.find('div' , class_='_1qfi0x77 instructor-count-display').text
-                    except:
-                        temp_json["Course Provider"] = ""
-
-
-            try:
-                temp_json["Course Description"] = lecture_details_json["@graph"][1]["hasCourseInstance"]["description"].split("...")[0]
-            except KeyError as e:
-                try:
-                    temp_json["Course Description"] = lecture_details_json["@graph"][1]["description"].split("...")[0]
-                except KeyError:
-                    temp_json["Course Description"] = ""
-
-
-            try:
-                temp_json["# of Students Enrolled"] = html.find('div', class_='rc-ProductMetrics').text.split(" ")[0]
-            except AttributeError:
-                try:
-                    temp_json["# of Students Enrolled"] =  html.find_all('div', class_='css-oj3vzs')[0].text.split(" ")[2]
-                except IndexError as e:
-                    temp_json["# of Students Enrolled"] = "0"
-
-
-            try:
-                temp_json["# of Ratings"] = lecture_details_json["@graph"][1]["aggregateRating"]["reviewCount"]
-            except KeyError as e:
-                try:
-                    temp_json["# of Ratings"] = html.find('div', class_= "_wmgtrl9 color-white ratings-count-expertise-style").text.split(" ")[0]
-                except AttributeError as e:
                     try:
-                        temp_json["# of Ratings"] = html.find_all('div', class_='css-oj3vzs')[0].text.split(" ")[0]
-                    except IndexError:
-                        temp_json["# of Ratings"] = "0.0"
+                        lecture_details_html = html.find('script', attrs={"type":"application/ld+json"}).text
+                    except AttributeError:
+                        try:
+                            lecture_details_html = html.find('div', class_='rc-CDPSchemaMarkup').find('script').text
+                        except:
+                            lecture_details_html = None
+                            
+            if lecture_details_html is not None:
+                
+                lecture_details_json = json.loads(lecture_details_html)
 
-            if temp_json["Course Name"]:
-                json_data["course_data"].append(temp_json)
+                
+                try:
+                    temp_json["Course Name"] = lecture_details_json["@graph"][1]["name"]
+                except KeyError as e:
+                    temp_json["Course Name"] = ""
+
+                try:
+                    temp_json["Course Provider"] = lecture_details_json["@graph"][1]["provider"]["name"]
+                except:
+                    try:
+                        temp_json["Course Provider"] = lecture_details_json["@graph"][1]["provider"][0]["name"]
+                    except KeyError as e:
+                        try: 
+                            temp_json["Course Provider"] = html.find('div' , class_='_1qfi0x77 instructor-count-display').text
+                        except:
+                            temp_json["Course Provider"] = ""
+
+
+                try:
+                    temp_json["Course Description"] = lecture_details_json["@graph"][1]["hasCourseInstance"]["description"].split("...")[0]
+                except KeyError as e:
+                    try:
+                        temp_json["Course Description"] = lecture_details_json["@graph"][1]["description"].split("...")[0]
+                    except KeyError:
+                        temp_json["Course Description"] = ""
+
+
+                try:
+                    temp_json["# of Students Enrolled"] = html.find('div', class_='rc-ProductMetrics').text.split(" ")[0]
+                except AttributeError:
+                    try:
+                        temp_json["# of Students Enrolled"] =  html.find_all('div', class_='css-oj3vzs')[0].text.split(" ")[2]
+                    except IndexError as e:
+                        temp_json["# of Students Enrolled"] = "0"
+
+
+                try:
+                    temp_json["# of Ratings"] = lecture_details_json["@graph"][1]["aggregateRating"]["reviewCount"]
+                except KeyError as e:
+                    try:
+                        temp_json["# of Ratings"] = html.find('div', class_= "_wmgtrl9 color-white ratings-count-expertise-style").text.split(" ")[0]
+                    except AttributeError as e:
+                        try:
+                            temp_json["# of Ratings"] = html.find_all('div', class_='css-oj3vzs')[0].text.split(" ")[0]
+                        except IndexError:
+                            temp_json["# of Ratings"] = "0.0"
+
+                if temp_json["Course Name"]:
+                    json_data["course_data"].append(temp_json)
     
     
 async def scrape_category_course_urls(url):
@@ -115,5 +125,5 @@ async def main(dynamic_url):
     time_difference = time.time() - start_time
     print(f'Scraping time: %.2f seconds.' % time_difference)
 
-#asyncio.run(main("business"))
+asyncio.run(main("math-and-logic"))
 
